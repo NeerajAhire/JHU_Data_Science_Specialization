d15b493f4b0b196827905765881ec45564166956
NeerajAhire <neerajahire@gmail.com>
2022-08-16 00:11:14 +0530

    Add files via upload

---
+++ Developing_Data_Products/Data_Product/server.R
@@ -0,0 +1,47 @@
+
+
+library(shiny)
+
+
+shinyServer(function(input, output) {
+    
+    model <- reactive({
+        brushed_data <- brushedPoints(swiss, input$brush1, xvar = "Education", yvar = "Fertility")
+        if(nrow(brushed_data) < 2){
+            return(NULL)
+        }
+        lm(Fertility ~ Education, data = brushed_data)
+    })
+    
+    output$slopeOut <- renderText({
+        if(input$show_values == FALSE){
+            return("") 
+        }
+        if(is.null(model())){
+            "No Model Found"
+        } else {
+            model()[[1]][2]}
+    })
+    
+    output$intOut <- renderText({
+        if(input$show_values == FALSE){
+            return("")
+        }
+        if(is.null(model())){
+            "No Model Found"
+        } else {
+            model()[[1]][1]
+        }
+    })
+    
+    output$plot1 <- renderPlot({
+        plot(swiss[, input$variable], swiss$Fertility, xlab = input$variable,
+             ylab = "Fertility", main = paste("Fertility as a measure of", input$variable, "(select required data points using brush)"),
+             cex = 1.5, pch = 16, bty = "n")
+        if(!is.null(model())){
+            abline(model(), col = "blue", lwd = 2)
+        }
+    })
+    
+})
+
---
+++ Developing_Data_Products/Data_Product/ui.R
@@ -0,0 +1,38 @@
+
+library(shiny)
+
+
+shinyUI(fluidPage(
+    
+    # Application title
+    titlePanel("Swiss Data Basic Exploration Plot"),
+    h3("The plot tab contains a plot of Fertility vs chosen input 'x' variable"),
+    selectInput("variable", "Variable:",
+                c("Agriculture" = "Agriculture",
+                  "Examination" = "Examination",
+                  "Education" = "Education",
+                  "Catholic" = "Catholic",
+                  "Infant Mortality" = "Infant.Mortality")),
+    checkboxInput("show_values", "Show/Hide Actual Slope & Intercept values", value = FALSE),
+    
+    sidebarLayout(
+        sidebarPanel(
+            h3("Slope"),
+            textOutput("slopeOut"),
+            h3("Intercept"),
+            textOutput("intOut")
+        ),
+        
+        # Show a the required plot
+        mainPanel(
+            tabsetPanel(type = "tabs", 
+                        tabPanel("Documentation", br(), h3("Choose desired x variable from drop down 
+                                                           and select data points using brush for linear model in the
+                                                           plot tab.
+                                                           Complete details of the dataset can be found from offical
+                                                           documentation.
+                                                           ")),
+                        tabPanel("Plot", br(), plotOutput("plot1", brush = brushOpts(id = "brush1"))))
+        )
+    )
+))
